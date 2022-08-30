591816f04d96acf6b3c65f253c99991c5dd5a22a
Manish Kumar Gupta <manishgu231@gmail.com>
2022-08-30 21:05:47 +0530

    Color pallete added Brush color picker functionality added

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -3,7 +3,12 @@ package com.example.drawingapp
 import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageButton
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.core.view.get
 import com.example.drawingapp.gitScriibleapp.DrawingView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.dialog_brush_size.*
@@ -11,11 +16,18 @@ import kotlinx.android.synthetic.main.dialog_brush_size.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var drawingView:DrawingView? = null
+    private var drawingView: DrawingView? = null
+    private var mImageButtonCurrentPaint: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val linearLayoutPaintColors = findViewById<LinearLayout>(R.id.ll_color_nav)
+
+        // Linear layout content can be used as an array.
+       // mImageButtonCurrentPaint = linearLayoutPaintColors[2] as ImageButton
+
 
         drawingView = findViewById(R.id.drawing_view)
         val sizeForBrush = drawingView?.setSizeForBrush(20.toFloat())
@@ -31,6 +43,7 @@ class MainActivity : AppCompatActivity() {
         var brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush Size:")
+
         val smallButton = brushDialog.ib_small_brush
         smallButton.setOnClickListener{
             drawingView?.setSizeForBrush(10.toFloat())
@@ -49,5 +62,23 @@ class MainActivity : AppCompatActivity() {
             brushDialog.dismiss()
         }
         brushDialog.show()
+    }
+
+    fun paintClicked (view: View){
+        if(view != mImageButtonCurrentPaint ){
+            val imageButton = view as ImageButton
+            val colorTag = imageButton.tag.toString()
+            drawingView?.setColor(colorTag)
+
+            imageButton.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            )
+
+            mImageButtonCurrentPaint?.setImageDrawable(
+                ContextCompat.getDrawable(this,R.drawable.pallet_normal)
+            )
+
+            mImageButtonCurrentPaint = view
+        }
     }
 }
